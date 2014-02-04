06bf86355b62b9299a454f610a29ec0f61c0515e
rails <rails@rails.(none)>
2014-02-04 20:35:43 +0530

    Updated ruby

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ActiveRecord::Base
-  has_attached_file :attach, :styles => { :large => "640*480", :medium => "300*300>", :thumb => "100*100>" }
+  has_attached_file :attach, :styles => { :large => "640*480", :medium => "300*300>", :thumb => "100*100>" },
+       :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+       :url => "/system/:attachment/:id/:style/:filename"
   validates :email, :presence => true, :email_format => true
   validates :name , :presence => true
 # validates_format_of :attach_file_name, :with => %r{\.(docx|doc|pdf)$}i,
