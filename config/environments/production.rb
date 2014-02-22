9e9bc73fca3664a39f4b3ab4398605d91a58bfb9
rails <rails@rails.(none)>
2014-02-22 11:24:47 +0530

    changed email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Dinemedia::Application.configure do
    config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'dine-media.com',
-    :user_name            => 'marketing@dine-media.com',
-    :password             => 'brother!@#$5',
+#    :domain               => 'gmail.com',
+    :user_name            => 'biltysoftware@gmail.com',
+    :password             => 'brother@123',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 end
